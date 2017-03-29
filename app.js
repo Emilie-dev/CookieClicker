7eff787c43db6c9a3155c63f7bac48478b18edbf
Chauvin <chauvin-e@orange.fr>
2017-03-29 19:40:39 +0200

    add a comment

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ $("#cookie").click(function() {
 	count += 1;
 	$("#message").html(count);
 	console.log(count);
-
+	
+// I can see the number of time I click on the photo in my html page.
 
 	for (var i = 3; i <= array.length; i++) {
 		var random = Math.floor(Math.random() * 3);
