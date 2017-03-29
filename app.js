d7f437618235a880a477a60a2634c95990aa528f
Chauvin <chauvin-e@orange.fr>
2017-03-29 19:37:05 +0200

    add some functions

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-$(".img-circle").click(function() {
-	
+var count = 0;
+var array = [
+	"Bravo!",
+	"Continu!",
+	"Encore!",
+	"Super!",
+];
 
+$("#cookie").click(function() {
+	count += 1;
+	$("#message").html(count);
+	console.log(count);
+
+
+	for (var i = 3; i <= array.length; i++) {
+		var random = Math.floor(Math.random() * 3);
+		console.log(array);
+	}
 });
